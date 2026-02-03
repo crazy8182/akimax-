df09f9da5e454f0fef7ffaf2c49336c26170debc
crazy8182 <crazygaming4694@gmail.com>
2026-02-03 18:10:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6859451629")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://mahesh12:<db_password>@cluster0.hscxg.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Mahesh8182)
